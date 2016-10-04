957c7498f1c9cee395ec7d2fabb5de6155f2f78b
marcelocell <marcelocell@users.noreply.github.com>
2016-10-04 08:52:02 -0300

    inclusao da funcao de selecao de musica

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+
+function confirmarSelecao(musica, artista, codigo) {
+    if (confirm("Selecionar a música \"" + musica + "\" de " + artista) == true) {
+        alert("Música selecionada! (SIMULAÇÂO)");
+    } 
+}
